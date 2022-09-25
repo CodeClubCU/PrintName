eb8262da3ac4d4ecb2a30b7721fb0b319d1be14d
shashank-iter <pandeyshashank812@gmail.com>
2022-09-25 14:51:48 +0530

    Name updated in Array

--- PrintName_Java.java
+++ PrintName_Java.java
@@ -8,7 +8,7 @@ public class PrintName_Java {
         //AND COMMIT THE CHANGES USING GIT TO YOUR REPOSITORY AND
         //THEN SEND A PULL REQUEST
         //AND GET YOUR FIRST PULL REQUEST MERGED
-        String[] NameArray = {"CodeClub CU"};
+        String[] NameArray = {"CodeClub CU","Shashank Pandey"};
         for(int i=0; i < NameArray.length; i++){
         System.out.print((i+1));
         System.out.println(") " + NameArray[i]);
