4d643fb6fdeb2f84f12c3f15f4abd27356112943
Shanatnu Raut <rshantanu73@gmail.com>
2022-10-01 02:06:15 +0530

    Update on Javascript file

--- PrintName_JavaScript.js
+++ PrintName_JavaScript.js
@@ -9,7 +9,7 @@ console.log("List of Members/Contributers:\n")
 //THEN SEND A PULL REQUEST
 //AND GET YOUR FIRST PULL REQUEST MERGED
 
-let arr = ["CodeClub CU"];
+let arr = ["CodeClub CU", "Shantanu raut"];
 
 for(let i = 0; i < arr.length; i++){
   console.log((i+1).toString() + ')',arr[i]);
