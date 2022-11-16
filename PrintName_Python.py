d671e8d2d58fd2307cfb71863ccbfade3b423954
Douwe Zumker <90844755+Peredurz@users.noreply.github.com>
2022-11-16 20:43:14 +0100

    Added my own first name to NameArray

--- PrintName_Python.py
+++ PrintName_Python.py
@@ -11,7 +11,7 @@ print("List of Members/Contributers:\n")
 #THEN SEND A PULL REQUEST
 #AND GET YOUR FIRST PULL REQUEST MERGED
 
-NameArray = ["CodeClub CU", "Ankan Halder","Gourav Kundu", "Amit Eshore"]
+NameArray = ["CodeClub CU", "Ankan Halder","Gourav Kundu", "Amit Eshore", "Douwe"]
 for i in range(len(NameArray)):
     print("%s)%s"%(i+1,NameArray[i]))
  
