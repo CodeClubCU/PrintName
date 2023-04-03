7969e73a3cea85f02850f745815b5ba6ccb97a45
ibtesam5d <ibtesam.hossain@gmail.com>
2023-04-03 11:48:52 -0600

    added my name to the array

--- PrintName_JavaScript.js
+++ PrintName_JavaScript.js
@@ -1,18 +1,23 @@
 //The PRIMARY OBJECTIVE OF THIS CODEBASE IS TO
 //LEARN TO USE GIT AND GITHUB AND SEND PULL REQUESTS
-console.log("      CodeCLub CU      \n")
+console.log("      CodeCLub CU      \n");
 
-console.log("List of Members/Contributers:\n")
+console.log("List of Members/Contributers:\n");
 
 //JUST ADD YOUR NAME IN THE ARRAY BELOW AS A STRING(make sure the code works as it should)
 //AND COMMIT THE CHANGES USING GIT TO YOUR REPOSITORY AND
 //THEN SEND A PULL REQUEST
 //AND GET YOUR FIRST PULL REQUEST MERGED
 
+let arr = [
+  "CodeClub CU",
+  "Amit Eshore",
+  "Jeremy Quihano",
+  "Cesar Avina",
+  "BuddhaHeadDev",
+  "Md Ibtesam Hossain",
+];
 
-let arr = ["CodeClub CU", "Amit Eshore","Jeremy Quihano", "Cesar Avina", "BuddhaHeadDev"];
-
-
-for(let i = 0; i < arr.length; i++){
-  console.log((i+1).toString() + ')',arr[i]);
+for (let i = 0; i < arr.length; i++) {
+  console.log((i + 1).toString() + ")", arr[i]);
 }
